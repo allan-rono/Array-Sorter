44c838dca74df0d62d57f75871b4dae83a62637f
Allan Rono <allanrono@hotmail.com>
2021-03-27 21:15:59 +1100

    Updated code snippet and read me file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Array-Sorter
 Author: Allan Rono
+
+# Array Sorting
+- By default, the JavaScript Array.sort function converts each element in the array that needs to be sorted into a string, and compares them in Unicode code point order.
+
+# Source
+https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
--- script.js
+++ script.js
@@ -4,10 +4,7 @@ const submitStrings =   document.getElementById("submitStrings");
 const answerStrings =   document.getElementById("answerStrings");
 const num =             document.getElementById("num");
 const submitNum =       document.getElementById("submitNum");
-const answerNum =       document.getElementById("answerNum");
-const objects =         document.getElementById("objects");
-const submitObjects =   document.getElementById("submitObjects");
-const answerObjects =   document.getElementById("answerObjects");
+const answerNum =       document.getElementById("answerNum")
 
 //Function to sort Input from user
 function sortStrings(){
@@ -29,12 +26,10 @@ function sortNumbers(){
         numbersArray[i] = Number(numbersArray[i]);
     }
     answerNum.innerHTML = numbersArray.sort();
-};
-function sortObjects(){};
+}
 
 //Event listeners
 submitStrings.addEventListener("click",sortStrings);
-submitNum.addEventListener("click", sortNumbers);
-submitObjects.addEventListener("click", sortObjects);
+submitNum.addEventListener("click", sortNumbers)
 
 
